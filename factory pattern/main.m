bd9852e2f1f170f3c9532684f4a6b3c98c7eb653
gaoweiwei <getfirstblood@gmail.com>
2012-12-11 20:03:23 +0800

    Initial Commit

---
+++ factory pattern/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  factory pattern
+//
+//  Created by gaoweiwei on 12-12-11.
+//  Copyright (c) 2012年 gaoweiwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
